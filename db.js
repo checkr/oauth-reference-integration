633f2e4f82de2138f433b3cdf5f90b314fc9030c
Solon Aguiar <solon.aguiar@checkr.com>
2022-12-22 13:31:54 -0800

    Remove s3 db

--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 import {Low, JSONFile, Memory} from 'lowdb'
 import testSeedData from './__tests__/testSupport/testSeedData.js'
 import seedData from './seedData.js'
-import S3Adapter from '@sadorlovsky/lowdb-s3'
 
 const testDB = async () => {
   const db = new Low(new Memory())
@@ -21,27 +20,7 @@ const devDB = async () => {
   return db
 }
 
-const prodDB = async () => {
-  const db = new Low(
-    new S3Adapter(
-      {bucket: process.env.S3_BUCKET, key: 'db.json'},
-      {
-        accessKeyId: process.env.S3_ACCESS_KEY_ID,
-        secretAccessKey: process.env.S3_ACCESS_KEY_SECRET,
-        region: 'us-east-1',
-      },
-    ),
-  )
-  await db.read()
-  if (!db.data) {
-    db.data = seedData
-    await db.write()
-  }
-  return db
-}
-
 const database = async () => {
-  if (process.env.NODE_ENV === 'production') return await prodDB()
   if (process.env.NODE_ENV === 'test') return await testDB()
   return await devDB()
 }
